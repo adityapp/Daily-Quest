cdd090581872ea8615fcc1c156940845f793d907
adityapp <axceofficial@gmail.com>
2019-12-10 11:49:10 +0700

    bug fix for scan parent barcode

--- app/src/main/java/com/dailyquest/feature/children/scanUid/presenter/ScanUidPresenter.kt
+++ app/src/main/java/com/dailyquest/feature/children/scanUid/presenter/ScanUidPresenter.kt
@@ -1,5 +1,6 @@
 package com.dailyquest.feature.children.scanUid.presenter
 
+import android.util.Log
 import com.dailyquest.feature.children.scanUid.ScanUidPresenterContract
 import com.dailyquest.feature.children.scanUid.ScanUidViewContract
 import com.dailyquest.utils.Constants
@@ -14,16 +15,20 @@ class ScanUidPresenter(private val view: ScanUidViewContract) :
 
     override fun validateParentUid(parentUid: String, sourceActivity: String) {
 
-        firebaseDatabase.getReference(Constants.DATABASE_USER).child(parentUid)
+        firebaseDatabase.getReference(Constants.DATABASE_USER)
             .addListenerForSingleValueEvent(object : ValueEventListener {
                 override fun onCancelled(p0: DatabaseError) {
                     view.showFailedMessage("Barcode tidak valid!")
                 }
 
                 override fun onDataChange(p0: DataSnapshot) {
-                    when (sourceActivity) {
-                        "RoleActivity" -> view.navigateToLogin(parentUid)
-                        "RegisterActivity" -> view.navigateToRegister(parentUid)
+                    if (p0.child(parentUid).exists()){
+                        when (sourceActivity) {
+                            "RoleActivity" -> view.navigateToLogin(parentUid)
+                            "RegisterActivity" -> view.navigateToRegister(parentUid)
+                        }
+                    }else{
+                        view.showFailedMessage("Barcode tidak valid!")
                     }
                 }
             })
